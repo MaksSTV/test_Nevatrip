9c5d512e6c59abc3bb25873da173c33a8df8430d
Максим Стрельников <maksim_stv@mail.ru>
2022-10-16 16:31:45 +0300

    done task-1

---
+++ task-1/code.js
@@ -0,0 +1,83 @@
+
+document.getElementsByClassName('item_1_price_900_1')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_1_900').style.display="block";
+        document.getElementById('item_1_1200').style.display="block";
+        document.getElementById('item_1_2900').style.display="none";
+        document.getElementById('item_1_4900').style.display="none";
+    });
+
+
+document.getElementsByClassName('item_1_price_900_2')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_1_900').style.display="block";
+        document.getElementById('item_1_1200').style.display="block";
+        document.getElementById('item_1_2900').style.display="none";
+        document.getElementById('item_1_4900').style.display="none";
+    });
+
+document.getElementsByClassName('item_1_price_2900_1')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_1_900').style.display="none";
+        document.getElementById('item_1_1200').style.display="none";
+        document.getElementById('item_1_2900').style.display="block";
+        document.getElementById('item_1_4900').style.display="none";
+    });
+
+document.getElementsByClassName('item_1_price_2900_2')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_1_900').style.display="none";
+        document.getElementById('item_1_1200').style.display="none";
+        document.getElementById('item_1_2900').style.display="block";
+        document.getElementById('item_1_4900').style.display="none";
+    });
+
+    document.getElementsByClassName('item_1_price_4900_1')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_1_900').style.display="none";
+        document.getElementById('item_1_1200').style.display="none";
+        document.getElementById('item_1_2900').style.display="none";
+        document.getElementById('item_1_4900').style.display="block";
+    });
+
+document.getElementsByClassName('item_1_price_4900_2')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_1_900').style.display="none";
+        document.getElementById('item_1_1200').style.display="none";
+        document.getElementById('item_1_2900').style.display="none";
+        document.getElementById('item_1_4900').style.display="block";
+    });
+
+/////////////////////////////
+
+document.getElementsByClassName('item_2_price_900_1')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_2_900').style.display="block";
+        document.getElementById('item_2_1200').style.display="block";
+        document.getElementById('item_2_2900').style.display="none";
+
+    });
+
+
+document.getElementsByClassName('item_2_price_900_2')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_2_900').style.display="block";
+        document.getElementById('item_2_1200').style.display="block";
+        document.getElementById('item_2_2900').style.display="none";
+    });
+
+document.getElementsByClassName('item_2_price_2900_1')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_2_900').style.display="none";
+        document.getElementById('item_2_1200').style.display="none";
+        document.getElementById('item_2_2900').style.display="block";
+    });
+
+document.getElementsByClassName('item_2_price_2900_2')[0]
+    .addEventListener('click', function (event) {
+        document.getElementById('item_2_900').style.display="none";
+        document.getElementById('item_2_1200').style.display="none";
+        document.getElementById('item_2_2900').style.display="block";
+    });
+
+    
